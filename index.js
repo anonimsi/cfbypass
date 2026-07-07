df6352fb3a4672f60188ac9fd01c2870ed6ebddb
anonimsi <tua46692@gmail.com>
2026-07-08 00:21:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -704,7 +704,7 @@ app.post('/session/clean', auth, init, (req, res) => {
 });
 app.get('/', (req, res) => {
   res.send(`<title> Halaman Kosong</> <h1>Lu Nyasar Apa Gimana Bang Wkwkwk</h1>`);
-)};
+});
 // Status
 app.get('/status', auth, init, (req, res) => {
     res.json({
